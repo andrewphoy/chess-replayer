0630b0a7d6ce29d277c1e400f222b8f3e6cb72df
Andrew Hoy <andrewphoy@gmail.com>
2012-09-11 14:44:14 -0400

    Added MIT License

--- js/jquery.chess-replayer.js
+++ js/jquery.chess-replayer.js
@@ -9,12 +9,27 @@
 
 /**
 *@license Chess Replayer 1.1.3
+*
+* Copyright (c) 2012 Andrew Hoy
 * 
-* Copyright (c) 2012 Andrew Hoy.  All rights reserved.
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the 
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
 * 
-* This library has not been released to the public.  If you are interested
-* in using jquery.chess-replayer, please contact Andrew Hoy at
-* andrewphoy@gmail.com for more information.
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 /** @define {boolean} */
